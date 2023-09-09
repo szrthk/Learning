b992fa3ffaa1f970f22c5c24cd2506059ad1e718
szrthk <143374809+szrthk@users.noreply.github.com>
2023-09-10 01:31:42 +0530

    new entry

---
+++ Java/practice/switchh.java
@@ -0,0 +1,12 @@
+package Java.practice;
+
+public class switchh {
+    public static void main (String[] args){
+
+        System.out.println("Today we are here to create a menu.");
+        
+        char 1;
+
+
+    }
+}
